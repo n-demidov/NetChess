c054721746f17148b8923ea540f5040df48cfa73
n-demidov <you@example.com>
2016-11-20 17:42:07 +0400

    Refactoring: extract Invitation to separate class 'Invitation'.

---
+++ NetChessParent/NetChessServer/src/main/java/edu/demidov/netchess/server/model/invitations/Invitation.java
@@ -0,0 +1,74 @@
+package edu.demidov.netchess.server.model.invitations;
+
+import edu.demidov.netchess.server.model.users.User;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Invitation
+{
+    private final User sourceUser;
+    private final Date invitedDate;
+
+    public Invitation(final User sourceUser, final Date invitedDate)
+    {
+        assert sourceUser != null;
+        assert invitedDate != null;
+
+        this.sourceUser = sourceUser;
+        this.invitedDate = invitedDate;
+    }
+
+    public User getSourceUser()
+    {
+        return sourceUser;
+    }
+
+    public Date getInvitedDate()
+    {
+        return invitedDate;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Invitation{" + "sourceUser=" + sourceUser + ", invitedDate=" + invitedDate + '}';
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 7;
+        hash = 61 * hash + Objects.hashCode(this.sourceUser);
+        hash = 61 * hash + Objects.hashCode(this.invitedDate);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+
+        final Invitation other = (Invitation) obj;
+        if (!Objects.equals(this.sourceUser, other.sourceUser))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.invitedDate, other.invitedDate))
+        {
+            return false;
+        }
+        return true;
+    }
+}
--- NetChessParent/NetChessServer/src/main/java/edu/demidov/netchess/server/model/invitations/InvitationManager.java
+++ NetChessParent/NetChessServer/src/main/java/edu/demidov/netchess/server/model/invitations/InvitationManager.java
@@ -10,7 +10,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Objects;
 import java.util.Set;
 
 import org.slf4j.Logger;
@@ -280,72 +279,5 @@ public class InvitationManager implements InvitationObservable
         final Date expiredDate = calendar.getTime();
 
         return expiredDate.before(Calendar.getInstance().getTime());
-    }
-    
-    private static class Invitation
-    {
-        private final User sourceUser;
-        private final Date invitedDate;
-
-        public Invitation(final User sourceUser, final Date invitedDate)
-        {
-            assert sourceUser != null;
-            assert invitedDate != null;
-            
-            this.sourceUser = sourceUser;
-            this.invitedDate = invitedDate;
-        }
-
-        public User getSourceUser()
-        {
-            return sourceUser;
-        }
-
-        public Date getInvitedDate()
-        {
-            return invitedDate;
-        }
-
-        @Override
-        public String toString()
-        {
-            return "Invitation{" + "sourceUser=" + sourceUser + ", invitedDate=" + invitedDate + '}';
-        }
-
-        @Override
-        public int hashCode()
-        {
-            int hash = 7;
-            hash = 61 * hash + Objects.hashCode(this.sourceUser);
-            hash = 61 * hash + Objects.hashCode(this.invitedDate);
-            return hash;
-        }
-
-        @Override
-        public boolean equals(Object obj)
-        {
-            if (this == obj)
-            {
-                return true;
-            }
-            if (obj == null)
-            {
-                return false;
-            }
-            if (getClass() != obj.getClass())
-            {
-                return false;
-            }
-            final Invitation other = (Invitation) obj;
-            if (!Objects.equals(this.sourceUser, other.sourceUser))
-            {
-                return false;
-            }
-            if (!Objects.equals(this.invitedDate, other.invitedDate))
-            {
-                return false;
-            }
-            return true;
-        }
     }
 }
